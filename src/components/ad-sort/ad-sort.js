10c7cba92b8cfc6460192cde3eb7231e00308e32
Norman Coloma García <ua.norman@gmail.com>
2019-07-07 17:02:19 +0200

    Fixing hidding sort

--- src/components/ad-sort/ad-sort.js
+++ src/components/ad-sort/ad-sort.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './ad-sort.css';
 
 const AdSort = ({ sortOpts, sortSelected,  handleSortOptClick}) => {
-    const handleFilterClick = (e) => {
-        if (e.target.tagName === "SPAN" || e.target.tagName === "path" || e.target.tagName === "BUTTON") {
-          const sortList = document.querySelector('.select-filter');
-          sortList.classList.toggle('show');
-        } else {
-          const sortList = document.querySelector('.select-filter');
-          sortList.classList.toggle('show'); 
-        }
+    const handleFilterClick = (ref) => {
+        if (ref && ref.target && ref.target.closest(".sort-container")) {
+            const sortList = document.querySelector('.select-filter');
+            sortList.classList.toggle('show');
+        } else if(ref && ref.target) {
+            const sortList = document.querySelector('.select-filter');
+            sortList.classList.remove('show');
+        } 
     }
 
+    useEffect(() => {
+      document.addEventListener("mousedown", handleFilterClick);
+      return () => {
+        document.removeEventListener("mousedown", handleFilterClick);
+      };
+    });
+
     const sortOptions = sortOpts.map(it => <li key={it} onClick={() => handleSortOptClick(it)}><a>{it}</a></li>);
+    const sortRef = useRef(null);
+    handleFilterClick(sortRef);
 
     return (
-        <div>
-          <button className="btn-filter" onClick={handleFilterClick} >
+        <div className="sort-container" ref={sortRef}>
+          <button className="btn-filter">
               <i className="fas fa-filter"></i> 
               <span>Sorted by: <strong>{sortSelected}</strong></span>
               <ul className="select-filter">
